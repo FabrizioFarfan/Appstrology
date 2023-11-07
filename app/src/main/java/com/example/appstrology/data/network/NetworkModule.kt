50729dcd5c8fd580c479ef1d02940d7b82e51dca
FabrizioFarfan <farfanfabrizio96@gmail.com>
2023-11-07 11:53:03 +0100

    added logging interceptor

--- app/src/main/java/com/example/appstrology/data/network/NetworkModule.kt
+++ app/src/main/java/com/example/appstrology/data/network/NetworkModule.kt
@@ -6,6 +6,9 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
@@ -16,10 +19,11 @@ object NetworkModule {
 
     @Singleton
     @Provides
-    fun provideRetrofit(): Retrofit{
+    fun provideRetrofit(okHttpClient :OkHttpClient): Retrofit{
         return Retrofit
             .Builder()
             .baseUrl("https://newastro.vercel.app/")
+            .client(okHttpClient)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
     }
@@ -30,8 +34,20 @@ object NetworkModule {
     }
 
     @Provides
+    @Singleton
+    fun provideOkHttpClient(): OkHttpClient {
+        val interceptor = HttpLoggingInterceptor()
+        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY)
+
+        return OkHttpClient
+            .Builder()
+            .addInterceptor(interceptor)
+            .build()
+
+    }
+
+    @Provides
     fun provideRepository(apiService: HoroscopeApiService): Repository{
         return RepositoryImpl(apiService)
     }
-
 }
